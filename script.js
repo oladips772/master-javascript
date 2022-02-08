d09493d14fe6b75756010c30dbf1cdcc44eddf4f
oladips <oladips200@gmail.com>
2022-02-08 11:41:42 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,165 @@
+/** @format */
+
+//* strings
+//*? charAt() method
+// const str = "HELLO WORLD"
+// const res = str.charAt(0)
+// console.log(res);
+
+// ? Concat method
+// const str1 = "Hello "
+// const str2 = "World"
+// const res = str1.concat(str2);
+// console.log(res);
+
+// ? charcodeAt method
+// const str = "HELLO World"
+// const res = str.charCodeAt(0)
+// console.log(res);
+
+// ? endsWith method
+// const str = "hello world, welcome to the universe";
+// const res = str.endsWith("universe")
+// console.log(res);
+
+// ? starstwith method
+// const str = "hello world, welcome to the universe";
+// const res = str.startsWith("hello")
+// console.log(res);
+
+// ? fromcharcode method
+// const res = String.fromCharCode(35);
+// console.log(res);
+
+// ? indexof method
+// const str = "hello world, welcome to the universe"
+// const res = str.indexOf("welcome");
+// console.log(res);
+
+// ? localcopare method
+// const str = "ab"
+// const str2 = "cd"
+// const res = str.localeCompare(str2)
+// console.log(res);
+
+// ? includes method
+// const str = "hello world"
+// const res = str.includes(" ")
+// console.log(res);
+
+// ? lastindexof method
+// const str = "hello planet earth, you are a great planet";
+// const res = str.lastIndexOf("planet");
+// console.log(res);
+
+// ? match method
+// const str = "The rain in SPAIN, stays mainly in plain"
+// const res = str.match(/AIN/g)
+// console.log(res);
+
+// ? reapeat method
+// const str = "hello world"
+// const res =  str.repeat(2)
+// console.log(res);
+
+// ? slice method
+// const str = "hello world"
+// const res = str.slice(0, 3)
+// console.log(res);
+
+// ? substring method
+// const str = "hello world"
+// const res = str.substring(0, 7)
+// console.log(res);
+
+// ? substr method
+// const str = "hello world";
+// const res = str.substr(1, 7);
+// console.log(res);
+
+// ? toLoacleLowerCase method
+// const str = "HELLO WORLD"
+// const res = str.toLocaleLowerCase()
+// console.log(res);
+
+// ? tolowerCase
+// const str = "HELLO WORLD"
+// const res = str.toLowerCase()
+// console.log(res);
+
+// ? trim method
+// const str = "hello world";
+// const res = str.trim()
+// console.log(res);
+
+// ? tolocaleUppercase
+// const str = "hello world"
+// const res = str.toLocaleUpperCase()
+// console.log(res);
+
+// ? toupperCase
+// const str = "hello world"
+// const res = str.toUpperCase()
+// console.log(res);
+
+// * Arrays
+// ? concat method
+// const fruits = ["Bannana", "oranges", "mangoes"];
+// const vegies = ["tomatoes", "pepper", "onions"]
+// const newArray = fruits.concat(vegies)
+// console.log(newArray);
+
+// ? entries method
+// const fruits = ["Bannana", "oranges", "mangoes"];
+// const arrays = fruits.entries();
+// console.log(arrays);
+
+// // ? copywithin method
+// const fruits = ["Bannana", "oranges", "mangoes", "pears", "melon"];
+// console.log(fruits);
+// const newF = fruits.copyWithin(2, 0);
+// console.log(newF);
+
+// ? every method
+// const ages = [19, 22, 34, 55, 19,];
+// function checkAges(age) {
+//     return age >= 18;
+// }
+// console.log(ages.every(checkAges));
+
+//  ??fill method
+// const fruits = ["Bannana", "oranges", "mangoes","pineapple"];
+// console.log(fruits.fill("kiwi", 0,1));
+
+// ? find method
+// const ages = [11, 22, 34, 18, 55, 19];
+// function checkAges(age) {
+//   return age >= 18;
+// }
+// console.log(ages.find(checkAges));
+
+// ? foreach method
+// let ages = [19, 22, 34, 18, 55, 19];
+
+// function timesAge(age,index,arr) {
+//     arr[index] = age * 10
+// }
+
+// ages.forEach(timesAge);
+// console.log(ages);
+
+//  ? filter method
+// let ages = [19, 22, 34, 18, 55, 19];
+// function lessThan20(age) {
+//     return age <= 20
+// }
+
+// console.log(ages.filter(lessThan20));
+
+// ? findindex method
+// let ages = [29, 22, 34, 18, 55, 19];
+// function lessThan20(age) {
+//     return age <= 20
+// }
+
+// console.log(ages.findIndex(lessThan20));
